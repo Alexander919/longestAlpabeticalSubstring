8741e571c259a3ef18271489619828d348325298
Alexander <a.jegorov84@gmail.com>
2019-12-22 22:39:29 +0000

    working solution

--- src/Kata.java
+++ src/Kata.java
@@ -1,9 +1,45 @@
+import java.util.ArrayList;
+import java.util.Optional;
+
 public class Kata {
     public static void main(String[] args) {
-
+        //abcde
+        System.out.println(longestAlpabeticalSubstring("z"));
     }
+
     public static String longestAlpabeticalSubstring(String text) {
-        // Your code here
-        return "";
+        if (text.length() == 1) return text;
+        System.out.println(text);
+        char[] chars = text.toCharArray();
+        StringBuilder sub = new StringBuilder();
+
+        ArrayList<String> subs = new ArrayList<>();
+
+        for (int i = 1; i < chars.length; i++) {
+            char c1 = chars[i - 1];
+            char c2 = chars[i];
+
+            sub.append(c1);
+
+            if (c2 >= c1 && i == chars.length - 1) {
+                sub.append(c2);
+                subs.add(sub.toString());
+            }
+
+            if (c2 < c1) {
+                subs.add(sub.toString());
+                System.out.println(sub);
+                sub.setLength(0);
+            }
+
+            if (c2 < c1 && i == chars.length - 1) {
+                subs.add(Character.toString(c2));
+            }
+
+        }
+        System.out.println(sub);
+        System.out.println(subs);
+
+        return subs.stream().reduce((acc, v) -> v.length() > acc.length() ? v : acc).get();
     }
 }
