3e43d9345f5f90f6dca86c27a44470a2b1d6c974
Alexander <a.jegorov84@gmail.com>
2019-12-22 21:17:33 +0000

    method names added

--- src/Kata.java
+++ src/Kata.java
@@ -1,2 +1,9 @@
 public class Kata {
+    public static void main(String[] args) {
+
+    }
+    public static String longestAlpabeticalSubstring(String text) {
+        // Your code here
+        return "";
+    }
 }
